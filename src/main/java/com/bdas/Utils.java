02923bbc979f9f17ab78fd92a341341c1f1e3e7c
AbstarctElement <fcukthepop@gmail.com>
2018-03-21 12:54:43 +0300

    yo

--- src/main/java/com/bdas/Utils.java
+++ src/main/java/com/bdas/Utils.java
@@ -5,7 +5,7 @@ import org.apache.commons.codec.binary.Base64;
 public  class Utils{
     public static String encodeCredentials(String username, String password) {
         byte[] credentials = (username + ':' + password).getBytes();
-        return new String(Base64.encodeBase64(credentials));
+        return "Basic " + new String(Base64.encodeBase64(credentials));
     }
 
     public static void print(Object text) {
